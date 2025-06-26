70e095fa6177bced16d8f7012f83de75dc642d7f
mjunaidsays <mjunaid2282001@gmail.com>
2025-06-26 14:08:55 +0500

    Initial commit

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // Configuration file for base API and WebSocket URLs
 
-export const API_BASE_URL = 'https://dd30-221-132-116-194.ngrok-free.app';
-export const WS_BASE_URL = 'wss://dd30-221-132-116-194.ngrok-free.app'; 
+export const API_BASE_URL = 'https://6fb8-221-132-116-194.ngrok-free.app';
+export const WS_BASE_URL = 'wss://6fb8-221-132-116-194.ngrok-free.app'; 

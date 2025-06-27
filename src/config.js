b2d7dfbe4c6543106248f2fecf54ac6d1313f245
mjunaidsays <mjunaid2282001@gmail.com>
2025-06-27 14:32:46 +0500

    New changes

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuration file for base API and WebSocket URLs
 
-export const API_BASE_URL = 'https://d772-103-125-177-218.ngrok-free.app';
-export const WS_BASE_URL = 'wss://d772-103-125-177-218.ngrok-free.app';
+export const API_BASE_URL = 'https://38c4-221-132-116-194.ngrok-free.app';
+export const WS_BASE_URL = 'wss://38c4-221-132-116-194.ngrok-free.app';
 
 export const endpoints = {
   processAudio: (meetingId) => `${API_BASE_URL}/api/transcriptions/${meetingId}/process`,
